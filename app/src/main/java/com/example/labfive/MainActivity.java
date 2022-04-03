0b8306b2bdedec888da65f2b66ff6b30c170f762
Ashim-K-Shrestha <sashim19@tbc.edu.np>
2022-04-03 19:39:06 -0400

    Homework Completed

--- app/src/main/java/com/example/labfive/MainActivity.java
+++ app/src/main/java/com/example/labfive/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,41 +17,38 @@ public class MainActivity extends AppCompatActivity {
     // creating a constant for the intent extra
     public static final int TEXT_REQUEST = 1;
 
-    // initializing an object of class ShopList for storing the products
-    private ShopList items = new ShopList();
+    // variable for the count value
+    int count = 0;
 
-    //
-    public MainActivity() {
-    }
+    // defining a variable for the count value
+    private TextView count_view;
+    private Button count_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // assigning and referencing the view to the variable
+        count_view = findViewById(R.id.textView);
+        count_button = findViewById(R.id.button11);
+
+
+        // using anonymous function because function is declared after 'new' keyword instead of object name
+        // setting an onClick listener for the count button to increase the count when clicked
+        count_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // increment in the value of variable
+                count++;
+                // count+"" is done to convert into string
+                // setting the value of the counter to the textView
+                count_view.setText(count+"");
+            }
+        });
+
         // Fetching the intent from the second activity
         Intent intent = getIntent();
-
-        // if the instances are saved and the list hashmap object is not null then,
-        if ((savedInstanceState != null) && (savedInstanceState.getSerializable("list") != null)) {
-            // storing the element of object 'list' into new hashmap object 'l'
-            HashMap<String, Integer> l = (HashMap<String, Integer>) savedInstanceState.getSerializable("list");
-            // assigning the textView with the id
-            TextView tv = findViewById(R.id.textView);
-            // setting the text to blank
-            tv.setText("");
-            // creating a set of the keys only from 'l' object and storing into the 'k' variable
-            for (String k : l.keySet()) {
-                // getting the elements similar to 'k' elements from 'l' object and storing into the new variable
-                String s = l.get(k).toString() + " " + k + "\n";
-                // setting the textView content with the elements from the new variable
-                tv.setText(tv.getText() + s);
-                // adding each items into the 'items' hashmap object
-                for (int i = 0; i < l.get(k); i++) {
-                    items.addItem(k);
-                }
-            }
-        }
     }
 
     public void launchSecondActivity(View view) {
@@ -58,39 +56,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SecondActivity.class);
         // starting the second activity with results
         startActivityForResult(intent, TEXT_REQUEST);
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        // inserting a serializable value into the bundle using the list as the key and its elements as the values
-        savedInstanceState.putSerializable("list", items.getItems());
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // if the requestCode is equal to TEXT_REQUEST
-        if (requestCode == TEXT_REQUEST) {
-            // if the resultCode is equal to RESULT_OK
-            if (resultCode == RESULT_OK) {
-                // storing the
-                String item = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
-                items.addItem(item);
-            }
-            // executing the function created below for displaying the list of products
-            drawView();
-        }
-    }
-
-    public void drawView() {
-        HashMap<String, Integer> l = items.getItems();
-        TextView tv = findViewById(R.id.textView);
-        tv.setText("");
-        for (String k : l.keySet()) {
-            String s = l.get(k).toString() + " " + k + "\n";
-            tv.setText(tv.getText() + s);
-        }
     }
 }
